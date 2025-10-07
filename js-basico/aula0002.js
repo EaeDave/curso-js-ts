a6bf7381bfc2ca7d8c1f19772ed3d05f19513d51
EaeDave <d4vd.sv@gmail.com>
2025-10-07 12:57:45 -0300

    comentarios de codigo

---
+++ js-basico/aula0002.js
@@ -0,0 +1,11 @@
+// Isso é um comentário e o motor do JavaScript não interpreta.
+console.log('O comentário acima não será exibido');
+console.log('Linha 1');
+console.log('Linha 2'); // Comentário não será exibido
+console.log('Linha 3');
+console.log('Linha 4');
+
+/**
+ * Isso é um "comentário de bloco"
+ * Tudo isso será ignorado.
+ */
