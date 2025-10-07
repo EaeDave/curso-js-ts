fd0082608b29d178984ae1baeeb35f30658ce8a5
EaeDave <d4vd.sv@gmail.com>
2025-10-07 19:54:42 -0300

    let vs var - primeira diferenca

---
+++ js-basico/aula0006/aula0006.js
@@ -0,0 +1,8 @@
+// ECMAScript - Padronização do JS
+var nome = 'David'; // Não usar
+var nome = 'Rodrigues'; // Está sendo possível redeclarar utilizando var
+console.log(nome);
+
+let idade = 23;
+// let idade = 14; Não é possível redeclarar com let
+console.log(idade);
