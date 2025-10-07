8ac3606defb8e5a2ac6206e0a0f6e0b3d47dfde3
EaeDave <d4vd.sv@gmail.com>
2025-10-07 12:48:29 -0300

    ex0001: exibir resultado no console

---
+++ js-basico/ex0001.js
@@ -0,0 +1,7 @@
+// Exiba a seguinte frase substituindo os campos, onde "X" tem que ser um Number:
+// Meu nome é "NOME". Estou aprendendo JavaScript às "X" da manhã/tarde/noite
+console.log(
+  'Meu nome é David. Estou aprendendo JavaScript às',
+  12,
+  'da tarde.'
+);
