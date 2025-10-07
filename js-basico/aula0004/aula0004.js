09140d287d457932ec1b6e849c97215133f86215
EaeDave <d4vd.sv@gmail.com>
2025-10-07 19:14:26 -0300

    variaveis com let

---
+++ js-basico/aula0004/aula0004.js
@@ -0,0 +1,27 @@
+// Muita repetição de nome
+// console.log('João nasceu em 1984.');
+// console.log('Em 2000 João conheceu Maria.');
+// console.log('João casou-se com Maria em 2012.');
+// console.log('Maria teve 1 filho com João em 2015.');
+// console.log('O filho de João se chama Eduardo.');
+
+// Variáveis armazenam valor na memória.
+let nome = 'João'; // Variável declarada e inicializada.
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com', nome, 'em 2015.');
+console.log('O filho de', nome, 'se chama Eduardo.');
+
+/**
+ * Convenções e regras de nomenclaturas de variáveis:
+ * Não pode criar com palavras reservadas
+ * Precisam ter nomes significativos
+ * Não pode começar com número
+ * Não podem conter espaços e ou traços
+ * Utiliza-se camelCase
+ * Case-sensitive
+ * Não pode redeclarar variáveis com let
+ * NÃO UTILIZE VAR, UTILIZE LET.
+ */
