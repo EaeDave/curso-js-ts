a712f88305974e4ea1f49d84b626d6828e66c686
EaeDave <d4vd.sv@gmail.com>
2025-10-08 18:03:41 -0300

    operadores aritmeticos, de atribuicao e incremento

---
+++ js-basico/aula0008/aula008.js
@@ -0,0 +1,26 @@
+/**
+ * Aritméticos
+ * + Adição / Concatenação
+ * - Subtração
+ * / Divisão
+ * * Multiplicação
+ */
+
+/**
+ * Ordem de prescedência
+ * ()
+ * **
+ * * / %
+ * + -
+ */
+
+const num1 = 2;
+const num2 = 10;
+console.log(`A soma entre ${num1} + ${num2} = ${num1 + num2}`);
+console.log(`A subração entre ${num1} - ${num2} = ${num1 - num2}`);
+console.log(`A multiplicação entre ${num1} * ${num2} = ${num1 * num2}`);
+console.log(`A divisão entre ${num1} / ${num2} = ${num1 / num2}`);
+
+let contador = 1;
+contador++; // Operador de incremento
+console.log(contador); // 2
