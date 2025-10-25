24c099aff4c8de2e5215e7146df20e4118663ba7
EaeDave <d4vd.sv@gmail.com>
2025-10-25 13:41:19 -0300

    if, else if e else (2)

---
+++ js-logica/aula0021/aula0021.js
@@ -0,0 +1,11 @@
+const numero = 10;
+
+if (numero >= 0 && numero <= 5) {
+	console.log('Sim, o número está entre 0 e 5.');
+} else {
+	console.log('O número não está entre 0 e 5.');
+}
+
+console.log(
+	'Isso será executado independentemente do if, pois está fora do bloco.',
+);
