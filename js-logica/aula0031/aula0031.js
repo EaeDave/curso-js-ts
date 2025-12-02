568bc98bd4e4a949ec4231938bb83a9ab69c838a
EaeDave <d4vd.sv@gmail.com>
2025-12-02 18:20:18 -0300

    while e do while - estrutura de repeticao

---
+++ js-logica/aula0031/aula0031.js
@@ -0,0 +1,20 @@
+const qtdVezesRepetirOLaco = 10;
+let repeticaoInicial = 1;
+
+while (repeticaoInicial <= qtdVezesRepetirOLaco) {
+  console.log(repeticaoInicial);
+  repeticaoInicial++;
+}
+
+function random(min, max) {
+  const r = Math.random() * (max - min) + min;
+  return Math.floor(r);
+}
+
+let rand = random(1, 50);
+console.log(rand);
+
+do {
+  console.log('OI');
+  rand = random(1, 50);
+} while (rand != 10);
